21b89cc015b99bc18dfcb6226ac1a44d082c6978
Sandu <1103324414@qq.com>
2019-05-14 21:11:46 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask import url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return "Welcome to My Watchlist!"
-    
+    return 'hello!'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User %s!' % name
+
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='greyli'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for',num=2))
+    return 'Test page!'
