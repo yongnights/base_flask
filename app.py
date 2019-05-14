903886043f8a0d30c9ac17a112894d9867e8fcea
Sandu <1103324414@qq.com>
2019-05-14 22:15:02 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
 
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+
+
+class User(db.Model): # 表名将会是 user（自动生成，小写处理）
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    name = db.Column(db.String(20)) # 名字
+
+class Movie(db.Model): # 表名将会是 movie
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True) # 主键
+    title = db.Column(db.String(60)) # 电影标题
+    year = db.Column(db.String(4)) # 电影年份
+
 
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -36,4 +51,43 @@ def test_url_for():
 
 
 
+# 生成虚拟数据
+import click
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop: # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
 
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+
+    click.echo('Done.')
